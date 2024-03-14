144018faf59383f84d894595fac5a7cc29211b80
Tarlis Portela <tarlis@tarlis.com.br>
2024-03-14 23:50:12 +0000

    aula 14/03

---
+++ exe02.py
@@ -0,0 +1,11 @@
+print(":: Programa 03 de 14/03/24 ::")
+idade = int( input("Informe sua idade: ") )
+
+if idade <= 12:
+    print("Tu es Criança")
+elif idade < 18:
+    print("Tu es Adolescente")
+else:
+    print("Adulto")
+
+print("Sua idade é:", idade)
